34b40aee017366d5ae5db6e01717ccdd60a01f75
Disciplefov <93633459+Disciplefov@users.noreply.github.com>
2021-11-02 23:51:29 -0400

    Update Guest.java

--- HotelBackEnd/src/Guest.java
+++ HotelBackEnd/src/Guest.java
@@ -1,7 +1,7 @@
 public class Guest {
-    String name;
-    String phone;
-    String idCard;
+    String name; // Guest's name
+    String phone; // Guest's phone
+    String idCard; // Guest's id
 
     Guest(String input_name, String input_phone, String input_idCard) {
         this.name = input_name;
